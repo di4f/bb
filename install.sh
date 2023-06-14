1650f26ef93a17b048759a58cc703125f49a34f5
surdeus <surdeus@gmail.com>
2023-06-14 20:02:06 +0300

    The goblin must be easily installable without itself.

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@
 
 wd=`pwd`
 
-cd $wd/src/cmd/goblin && go install && cd $wd
+# cd $wd/src/cmd/goblin && go install && cd $wd
+go install
 
 mkdir -p $HOME/app/goblin
 cp -rf $wd/app/* $HOME/app/goblin
